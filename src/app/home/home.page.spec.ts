ba18780b3f98d8ce237d1b27a79c67922669391e
Marco Canu <nome.marco@icloud.com>
2023-07-12 17:17:33 +0200

    added routes

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { Router } from '@angular/router';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +17,20 @@ describe('HomePage', () => {
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
+    router=TestBed.get(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go on pick-up calls on see all', () => {
+    spyOn(router,'navigate');
+    component.goToPickUpCalls();
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-calls'])
   });
+  it('should go new pick-up calls on create pickup-call', () => {
+    spyOn(router,'navigate');
+    component.newPickUpCall();
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-call'])
+  });
+
 });
+
--- src/app/pages/register/register.page.spec.ts
+++ src/app/pages/register/register.page.spec.ts
@@ -1,17 +1,27 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RegisterPage } from './register.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let router:Router;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegisterPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
     fixture = TestBed.createComponent(RegisterPage);
     component = fixture.componentInstance;
+    router=TestBed.get(Router);
     fixture.detectChanges();
-  }));
+  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to homePage in register', () => {
+    spyOn(router,'navigate');
+    component.register();
+    expect(router.navigate).toHaveBeenCalledWith(['home'])
   });
 });
